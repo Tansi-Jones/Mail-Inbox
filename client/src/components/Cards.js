497451073a217921f535daee8fb408b3a6923203
tansi-jones <tansijones@outlook.com>
2022-06-16 15:15:18 +0300

    Update Cards.js

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Cards = ({ timeStamp, subject, content, isRead }) => {
+export const Cards = ({ timeStamp, subject, content, isRead, id }) => {
   const [contents] = useState(
     "It is a long established fact that a reader will be distracted"
   );
+
+  const navigate = useNavigate();
 
   const trim = () => {
     let length = 40;
@@ -18,6 +21,7 @@ export const Cards = ({ timeStamp, subject, content, isRead }) => {
       className={`${
         isRead ? "unReadFlag" : "readFlag"
       } relative bg-white rounded-lg px-3 py-5 my-2 flex justify-between shadow-sm hover:shadow transition duration-200 ease-in-out cursor-pointer`}
+      onClick={() => navigate(`/mail/${id}`)}
     >
       <div className="flex items-center space-x-5">
         <div className="w-10 h-10 rounded-full">
